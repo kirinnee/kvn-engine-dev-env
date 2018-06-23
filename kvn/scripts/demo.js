2e4f7024956d0045cb4202daf3a3de7e52fffc4a
Kirinne <kirinnee97@gmail.com>
2018-06-24 00:42:03 +0800

    - updated example

--- kvn/scripts/demo.js
+++ kvn/scripts/demo.js
@@ -1,15 +1,29 @@
 let demo =  new Scene("demo",
 [new Frame(function(){
 	stage.bringCharacter(sophie);
+	stage.bringCharacter(kvnfolder);
+	stage.bringCharacter(imagefolder);
 	stage.display(null,null,1000,function() {
-		sophie.appear(500);
+		sophie.changeSprite('smile',function() {
+			sophie.appear(500);
+		});
 	});
 }, function(){return null})
 ,new Frame(function(){
-	sophie.speak('Hi, this is an example on how to use the KVN engine');
+	sophie.changeSprite(def,function() {
+		sophie.speak('Hi!',function() {
+			sophie.wait(300,function() {
+				sophie.contSpeaking('I\'m Sophie!',function() {
+					sophie.wait(300,function() {
+						sophie.contSpeaking('And this is an example for the KVN engine~!!');
+					});
+				});
+			});
+		});
+	});
 }, function(){return null})
 ,new Frame(function(){
-	sophie.changeSprite('smile',function() {
+	sophie.changeSprite('overjoyed',function() {
 		sophie.speak('It\'s designed for non-programmers to code!');
 	});
 }, function(){return null})
@@ -28,16 +42,91 @@ let demo =  new Scene("demo",
 	});
 }, function(){return null})
 ,new Frame(function(){
-	sophie.speak('For starters, lets see where you put your images and scripts');
+	sophie.speak('For starters, lets get familiar with the workspace');
 }, function(){return null})
 ,new Frame(function(){
-	sophie.speak('Backgrounds goes into images/bkgd/ folder');
+	sophie.speak('Please open up the kvn folder in the workspace!',function() {
+		sophie.wait(500,function() {
+			sophie.changeSprite('shocked',function() {
+				sophie.speak('Ohh!',function() {
+					sophie.wait(800,function() {
+						sophie.changeSprite('smile',function() {
+							sophie.speak('Workspace refers to the folder you have downloaded!');
+						});
+					});
+				});
+			});
+		});
+	});
 }, function(){return null})
 ,new Frame(function(){
-	sophie.contSpeaking('and characters goes into image/char folder!');
+	sophie.changeSprite(def,function() {
+		sophie.speak('Anyways,',function() {
+			sophie.wait(200,function() {
+				sophie.contSpeaking('there are a few folders you need to be familiar with!');
+			});
+		});
+	});
 }, function(){return null})
 ,new Frame(function(){
-	sophie.speak('Scripts, on the other hand, goes into scripts/ folder!');
+	sophie.speak('Hmmm..');
+	sophie.changeSprite('unhappy',function() {
+		sophie.triggered(1000,function() {
+			sophie.changeSprite('smile',function() {
+				sophie.speak('There!');
+				sophie.move(-20,null,500,function() {
+					kvnfolder.appear(500,function() {
+						sophie.contSpeaking('This is how the kvn folder in your workspace should look like!');
+					});
+				});
+			});
+		},null,null,195);
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.speak('Alright!',function() {
+		sophie.wait(200,function() {
+			sophie.changeSprite('overjoyed',function() {
+				sophie.contSpeaking('Let\'s get started~!!');
+			});
+		});
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.changeSprite(def,function() {
+		sophie.speak('This is the images folder!',function() {
+			sophie.wait(300,function() {
+				sophie.contSpeaking('This is where we place all our images');
+			});
+		});
+	});
+	kvnfolder.changeSprite('image');
+}, function(){return null})
+,new Frame(function(){
+	sophie.changeSprite('overjoyed',function() {
+		sophie.speak('I will explain a little more later!');
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.changeSprite(def,function() {
+		kvnfolder.changeSprite('script');
+		sophie.speak('Next,',function() {
+			sophie.wait(300,function() {
+				sophie.contSpeaking('This is the scripts folder, where we put all our scripts!');
+			});
+		});
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.speak('and...');
+}, function(){return null})
+,new Frame(function(){
+	kvnfolder.changeSprite('sound',function() {
+		sophie.speak('This is where we place all our music or sound files!');
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.speak('It\'s quite simple!');
 }, function(){return null})
 ,new Frame(function(){
 	sophie.changeSprite('proud',function() {
@@ -45,6 +134,65 @@ let demo =  new Scene("demo",
 	});
 }, function(){return null})
 ,new Frame(function(){
-	sophie.speak('Well, that\'s it for today!');
+	sophie.speak('Moving on, lets open the image folder!');
+	kvnfolder.changeSprite('image');
+}, function(){return null})
+,new Frame(function(){
+	kvnfolder.disappear(200,function() {
+		imagefolder.appear(200,function() {
+			sophie.speak('This is how the image folder look like!');
+		});
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.changeSprite('shy',function() {
+		sophie.speak('It\'s pretty self-explanatory, but I will explain anyways!');
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.changeSprite(def,function() {
+		imagefolder.changeSprite('char',function() {
+			sophie.speak('This is the character folder');
+		});
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.speak('You should place all your character images inside!');
+}, function(){return null})
+,new Frame(function(){
+	sophie.speak('You can create subfolders and directories! For example, my default sprite is def.png!');
+}, function(){return null})
+,new Frame(function(){
+	sophie.speak('Moving on...',function() {
+		imagefolder.changeSprite('bkgd');
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.speak('This is the background folder.',function() {
+		sophie.wait(300,function() {
+			sophie.contSpeaking('It works pretty much like character folder, except its for the stage\'s background!');
+		});
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.speak('Hmm...',function() {
+		sophie.glitch();
+		sophie.changeSprite('overjoyed',function() {
+			sophie.contSpeaking('I think that\'s it for today!',function() {
+				sophie.wait(300,function() {
+					sophie.contSpeaking('My time is almost up...');
+				});
+			});
+		});
+	});
+}, function(){return null})
+,new Frame(function(){
+	sophie.speak('Well, see you around!',function() {
+		sophie.disappear(2000);
+		imagefolder.disappear(2000);
+	});
+}, function(){return null})
+,new Frame(function(){
+	stage.unDisplay(2000);
 }, function(){return null})
 ]);
--- kvn/scripts/init.js
+++ kvn/scripts/init.js
@@ -1,19 +1,30 @@
-
-
 function soundLoadPhase() {
-    //load/create sounds here
-
 }
 
-
-function publicStaticVoidMain(id) {
-
-    if (id === 0) {
-        //the first scene to be played here
-        //playScene(scene,frame);
-        playScene("demo",0);    }
-
+function publicStaticVoidMain(id){
+if(id===0){
+	playScene('demo',0);
 }
-
-
-//recommended to load construct reusable characters here
+}
+window.sophie = new Character('sophie','Sophie','def.png',30,30*(4960/1756),0,60,50,50);
+sophie.addSprite('overjoyed','overjoy.png');
+sophie.addSprite('proud','proud.png');
+sophie.addSprite('shy','shy.png');
+sophie.addSprite('smile','smile.png');
+sophie.addSprite('shocked','shocked.png');
+sophie.addSprite('unhappy','unhappy.png');
+sophie.setDefaultSkippable(true);
+sophie.setDefaultAnimateInterpolation(swing);
+sophie.complete();
+window.kvnfolder = new Character('kvnfolder','n','folder.JPG',40,40*(184/464),15,0,50,50);
+kvnfolder.addSprite('script','scripts.JPG');
+kvnfolder.addSprite('sound','sound.JPG');
+kvnfolder.addSprite('config','config.JPG');
+kvnfolder.addSprite('image','images.JPG');
+kvnfolder.complete();
+window.imagefolder = new Character('imagefolder','n','imagefold.JPG',40,40*127/470,15,0,50,50);
+imagefolder.addSprite('char','char.JPG');
+imagefolder.addSprite('bkgd','bkgd.JPG');
+imagefolder.complete();
+window.stage = new Stage('stage','background.jpg');
+stage.complete();
