7607d8ce2c7a951965e812f87a909e4f764f307d
Kirinne <kirinnee97@gmail.com>
2018-06-23 13:18:54 +0800

    v 0.046 KVN Dev Env

---
+++ install-interpreter.bat
@@ -0,0 +1,5 @@
+cd %cd%
+@echo off
+call git clone https://github.com/kirinnee/kvn-interpreter.git
+call kvn-interpreter/install.bat
+pause
---
+++ update.bat
@@ -0,0 +1,4 @@
+cd %cd%
+call git fetch origin master
+call git pull origin master
+pause
